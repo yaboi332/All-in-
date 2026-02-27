1b974dd046f33f9b3f8ad31773557adcb155f53b
ethanfu868-creator <ethanfu868@gmail.com>
2026-02-26 19:51:09 -0500

    setup
    made gamemanager file + setup

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+  
+}
