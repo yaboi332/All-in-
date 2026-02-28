581592f998d80addcc8c0f88436b1e07576d463b
ethanfu868-creator <ethanfu868@gmail.com>
2026-02-28 00:31:20 -0500

    Movement
    Created character movement

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,8 +1,13 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    [SerializeField] private float time = 0.1f;
+    [SerializeField] private bool isPlayerTurn = true;
+    public bool IsPlayerTurn => isPlayerTurn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -16,6 +21,23 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+private void Start()
+    {
+        // Initialize game state or load resources here
+        Instantiate(Resources.Load<GameObject>("Prefabs/Player")).name = "Player";
+    }
+public void EndPlayerTurn()
+    {
+        isPlayerTurn = false;
+        StartCoroutine(WaitForTurns());
 
+    }
+    private IEnumerator WaitForTurns()
+    {
+        yield return new WaitForSeconds(time);
+        isPlayerTurn = true;
+    }
+
+   
   
 }
