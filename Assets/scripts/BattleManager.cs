9f2f9d1b10011bb3a200db93aa8c4bac333ae0af
ethanfu868-creator <ethanfu868@gmail.com>
2026-02-28 16:50:07 -0500

    Battle System
    Started Battle system

---
+++ Assets/scripts/BattleManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public class BattleManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+}
