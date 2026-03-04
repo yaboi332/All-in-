a63a3f70e1598ede03f6b056398c642c40aa1619
ethanfu868-creator <ethanfu868@gmail.com>
2026-03-04 01:36:38 -0500

    unit
    Made unit script and enemy/player placeholder

---
+++ Assets/scripts/Unit.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int health;
+    public int maxHealth;
+    public int level;
+    public int damage;
+}
