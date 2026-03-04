6553d8a0c22644d5943c382fba11b896b0502ba0
ethanfu868-creator <ethanfu868@gmail.com>
2026-03-04 00:22:09 -0500

    prototype scene
    added prefabs and base for characters to stand

--- Assets/scripts/BattleManager.cs
+++ Assets/scripts/BattleManager.cs
@@ -4,6 +4,8 @@ public class BattleManager : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public BattleState state;
     void Start()
@@ -14,8 +16,8 @@ public class BattleManager : MonoBehaviour
     void setupBattle()
     {
         // Setup the battle, e.g. spawn player and enemy
-       Instantiate(playerPrefab);
-       Instantiate(enemyPrefab);
+       Instantiate(playerPrefab, playerBattleStation.position, playerBattleStation.rotation);
+       Instantiate(enemyPrefab, enemyBattleStation.position, enemyBattleStation.rotation);
     }
     
     // Update is called once per frame
