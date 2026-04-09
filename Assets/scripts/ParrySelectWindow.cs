60d8a097e21deb9b20d3ed5192317fee3e353cf2
Nathan Josue <njosue1017@gmail.com>
2026-04-09 14:24:51 -0400

    Parry Select Window Fixes
    Fixed Units appearing in front of the Parry Select Window and fixed being able to use other buttons when Parry Select Window is open.

--- Assets/scripts/ParrySelectWindow.cs
+++ Assets/scripts/ParrySelectWindow.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 public class ParrySelectWindow : MonoBehaviour
 {
         public GameObject popUpBox;
+        public GameObject buttonBlocker;
     
     public void Start()
     {
         popUpBox.SetActive(false);
+        buttonBlocker.SetActive(false);
     }
 
     public void PopUp()
     {
         popUpBox.SetActive(true);
+        buttonBlocker.SetActive(true);
     }
 
     public void HidePopUp()
     {
         popUpBox.SetActive(false);
+        buttonBlocker.SetActive(false);
     }
 
 }
