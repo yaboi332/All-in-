6b1fcbdb78192b706789616d51d0c5da0bb09aa6
ethanfu868-creator <ethanfu868@gmail.com>
2026-04-15 20:59:57 -0400

    attack picker
    created window to pick attack for player

---
+++ Assets/scripts/AttackSelectWindow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AttackSelectWindow : MonoBehaviour
+{
+        public GameObject popUpBox;
+        public GameObject buttonBlocker;
+    
+    public void Start()
+    {
+        popUpBox.SetActive(false);
+        buttonBlocker.SetActive(false);
+    }
+
+    public void PopUp()
+    {
+        popUpBox.SetActive(true);
+        buttonBlocker.SetActive(true);
+    }
+
+    public void HidePopUp()
+    {
+        popUpBox.SetActive(false);
+        buttonBlocker.SetActive(false);
+    }
+
+}
